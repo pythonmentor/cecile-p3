7539ba87e5e624bf68cc05daa7c48803e6ada2cd
Cécile Gillet <cecile.gillet@jasparo.fr>
2019-03-24 16:32:09 +0100

    added tutorial-env

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
 import pygame
+from pygame.locals import *
 from app.position import Position
 from app.subapp.module import (
     Classe1, Classe2, fonction1, fonction2
 )
+
+pygame.init()
 
 def main():
     """Main entry point."""
