b3fa9589dfc68d40f03fb545a16b09d021cc7ee3
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-03-28 10:59:37 +0100

    Update objets.py

--- app/objets.py
+++ app/objets.py
@@ -1,3 +1,5 @@
+# -tc- De manière générale, nommer les classes au singulier avec une majuscule en début de mot.
+# -tc- Coder de préférence en anglais et le mot object est un mot réservé en python
 class objets:
     pass
 
